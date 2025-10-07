eacbca5ff5669ef619a25a7fa7f674d81f038a08
HardBoss07 <matteo@bosshard.net>
2025-10-07 16:18:07 +0200

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import GameGrid from "@/components/grid/GameGrid";
+
+export default function HomePage() {
+    return (
+        <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white p-8">
+            <GameGrid/>
+        </main>
+    );
+}
